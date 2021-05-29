71328ffc5764b4f4a5b0060b1178d62f80c6f0dc
reedniv <26852596+reedniv@users.noreply.github.com>
2021-05-29 22:24:47 +0700

    Update dice.js

--- dice/javascript/dice.js
+++ dice/javascript/dice.js
@@ -9,28 +9,24 @@
 // @downloadURL https://github.com/reedniv/script/blob/main/dice/javascript/dice.js
 // @updateURL https://github.com/reedniv/script/blob/main/dice/javascript/dice.js
 // ==/UserScript==
-var count_min = 1;
-(function() {
-console.log(document.readyState);
-          setTimeout(function(){
-       document.getElementsByClassName("Button__StyledButton-sc-8bd3dp-0 fbjzSA styles__Button-fc7ea4-0 mGJpP")[0].click();
-         }, random(2000,4000));
+var coin = "trx";
 
-      setTimeout(function(){
-       document.getElementsByClassName("Button__StyledButton-sc-8bd3dp-0 fbjzSA")[0].click();
-         }, random(2000,4000));
+window.onload = function loadpage(){
 
-    setInterval(function(){
-        console.log("Status: Elapsed time " + count_min + " minutes");
-        count_min = count_min + 1;
-    }, 60000);
+setInterval(function(){
+console.log("Status: Elapsed time " + count_min + " minutes");
+count_min = count_min + 1;
+}, 60000);
+    
+setInterval(function(){
+document.getElementsByClassName("Button__StyledButton-sc-8bd3dp-0 fbjzSA")[0].click();
+}, random(2000,4000));
 
-
-    setInterval(function(){
-               document.getElementsByClassName("Button__StyledButton-sc-8bd3dp-0 fbjzSA")[0].click()
-       }, random(622000,625000));
-})();
+setTimeout(function(){
+document.getElementsByClassName("Button__StyledButton-sc-8bd3dp-0 fbjzSA")[0].click();
+}, random(2000,3000));
+}
 
 function random(min,max){
-   return min + (max - min) * Math.random();
-};
+	return min + (max - min) * Math.random();
+}
